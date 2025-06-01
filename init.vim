be4b77a4b2cd4e54196d27a2297ecd17c73c5599
MyUsernamee <delanoleslieprof@gmail.com>
2025-06-01 03:02:19 -0700

    First C#, gdscript, tab stops

---
+++ init.vim
@@ -0,0 +1,8 @@
+lua require("config")
+
+call plug#begin()
+
+Plug 'vim-pandoc/vim-pandoc'
+Plug 'vim-pandoc/vim-pandoc-syntax'
+
+call plug#end()
---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+vim.opt.shiftwidth=4
+vim.opt.tabstop=4
+vim.opt.softtabstop=-1
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!:
