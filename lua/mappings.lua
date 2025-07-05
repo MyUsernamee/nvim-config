2e4e41243825d3a356bd3d7b243a691b2ecbd431
MyUsernamee <delanoleslieprof@gmail.com>
2025-07-05 12:26:43 -0700

    Add lsps

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -28,3 +28,16 @@ map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugge
 
 -- rustaceanvim
 map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+
+map("n", "<Up>", ":echoe 'Get off my lawn!'<CR>")
+map("n", "<Down>", ":echoe 'Get off my lawn!'<CR>")
+map("n", "<Left>", ":echoe 'Get off my lawn!'<CR>")
+map("n", "<Right>", ":echoe 'Get off my lawn!'<CR>")
+map("i", "<Up>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+map("i", "<Down>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+map("i", "<Left>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+map("i", "<Right>", "<C-o>:echoe 'Get off my lawn!'<CR>")
+map("v", "<Up>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+map("v", "<Down>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+map("v", "<Left>", ":<C-u>echoe 'Get off my lawn!'<CR>")
+map("v", "<Right>", ":<C-u>echoe 'Get off my lawn!'<CR>")
